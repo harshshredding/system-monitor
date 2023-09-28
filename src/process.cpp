a18484e030edd789412e03a6a494a37c315f5ee6
harshshredding <harshshredding@gmail.com>
2023-09-28 19:27:25 +0000

    display cpu utilization of each process

--- src/process.cpp
+++ src/process.cpp
@@ -6,16 +6,58 @@
 #include <fstream>
 
 #include "process.h"
+#include "helper.h"
+#include "linux_parser.h"
 
 using std::string;
 using std::to_string;
 using std::vector;
 
+
+vector<string> split_string(string str, char delimiter) {
+    std::vector<string> ret;
+    std::istringstream string_stream(str);
+    string token;
+    while(getline(string_stream, token, delimiter)) {
+        ret.push_back(token);
+    }
+    return ret;
+}
+
 // TODO: Return this process's ID
 int Process::Pid() { return this->process_id; }
 
 // TODO: Return this process's CPU utilization
-float Process::CpuUtilization() { return 0; }
+float Process::CpuUtilization() { 
+    std::string stat_file_path = "/proc/" + std::to_string(this->process_id) + "/stat";
+    std::ifstream file_stream(stat_file_path);
+    if (file_stream.is_open()) {
+        std::string line;
+        if (std::getline(file_stream, line)) {
+            vector<string> values = split_string(line, ' ');
+            string utime = values[13];
+            string stime = values[14];
+            string c_utime = values[15];
+            string c_stime = values[16];
+            long start_time = stol(values[21]);
+            long ticks_per_second = sysconf(_SC_CLK_TCK);
+            long total_time_spent = stol(utime) + stol(stime) + stol(c_utime) + stol(c_stime);
+            long uptime = LinuxParser::UpTime();
+            long process_time = uptime - (start_time/ticks_per_second);
+            float usage = 1.0*(total_time_spent/ticks_per_second)/(process_time);
+            return usage;
+            /* log("pid " + std::to_string(this->process_id)); */
+            /* log("usage " + to_string(usage)); */
+            /* log("ticks_per_second " + to_string(ticks_per_second)); */
+            /* log("start_time " + to_string(start_time)); */
+            /* log("c_stime " + c_stime); */
+            /* log("c_utime " + c_utime); */
+            /* log("stime " + stime); */
+            /* log("utime " + utime); */
+        }
+    }
+    return 0.0;
+}
 
 // TODO: Return the command that generated this process
 string Process::Command() {
@@ -73,15 +115,6 @@ string get_user_uuid(int pid) {
     return "NOT AVAILABLE";
 }
 
-vector<string> split_string(string str, char delimiter) {
-    std::vector<string> ret;
-    std::istringstream string_stream(str);
-    string token;
-    while(getline(string_stream, token, delimiter)) {
-        ret.push_back(token);
-    }
-    return ret;
-}
 
 string get_username(string uuid) {
     std::string passwd_file_path = "/etc/passwd";
