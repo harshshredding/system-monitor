a8f70c0bcfe713d95d95d83216b9542c70dbddfa
harshshredding <harshshredding@gmail.com>
2023-09-28 20:55:49 +0000

    format system uptime

--- src/ncurses_display.cpp
+++ src/ncurses_display.cpp
@@ -47,7 +47,7 @@ void NCursesDisplay::DisplaySystem(System& system, WINDOW* window) {
   mvwprintw(
       window, ++row, 2,
       ("Running Processes: " + to_string(system.RunningProcesses())).c_str());
-  mvwprintw(window, ++row, 2, ("Up Time: " + to_string(system.UpTime())).c_str());
+  mvwprintw(window, ++row, 2, ("Up Time: " + Format::ElapsedTime(system.UpTime())).c_str());
   wrefresh(window);
 }
 
