bcf1a86d3da3db1964833c0ea779d842ff67ff9d
harshshredding <harshshredding@gmail.com>
2023-09-29 18:38:47 +0000

    fix time formatting for single digit case

--- src/format.cpp
+++ src/format.cpp
@@ -9,9 +9,21 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long total_seconds[[maybe_unused]]) { 
+string Format::ElapsedTime(long total_seconds) { 
     int hours = total_seconds/3600;
     int minutes = (total_seconds%3600)/60;
     int seconds = total_seconds%60;
-    return to_string(hours) + ":" + to_string(minutes) + ":" + to_string(seconds);
+    string hours_string = to_string(hours);
+    string minutes_string = to_string(minutes);
+    string seconds_string = to_string(seconds);
+    if (minutes < 10) {
+        minutes_string = "0" + minutes_string;
+    }
+    if (seconds < 10) {
+        seconds_string = "0" + seconds_string;
+    }
+    if (hours < 10) {
+        hours_string = "0" + hours_string;
+    }
+    return hours_string + ":" + minutes_string + ":" + seconds_string;
 }
