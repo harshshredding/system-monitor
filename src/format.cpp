bdad49cfa2ffc89a220c733a69c9c1458880359f
harshshredding <harshshredding@gmail.com>
2023-09-29 18:43:21 +0000

    fix time formatting for hours

--- src/format.cpp
+++ src/format.cpp
@@ -13,6 +13,7 @@ string Format::ElapsedTime(long total_seconds) {
     int hours = total_seconds/3600;
     int minutes = (total_seconds%3600)/60;
     int seconds = total_seconds%60;
+    hours = hours % 100;
     string hours_string = to_string(hours);
     string minutes_string = to_string(minutes);
     string seconds_string = to_string(seconds);
